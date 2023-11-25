a7f583f104a9081e563fe8ffa93414744f84e2f0
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2023-11-25 17:15:20 +0700

    add azure functions and update requirement.txt

--- test.py
+++
@@ -1,6 +0,0 @@
-import math
-button_controller_length = 4
-current_page = 4
-test = current_page + 1 if current_page < button_controller_length else current_page
-print('test', test)
-print('current_page < button_controller_length', current_page < button_controller_length)
